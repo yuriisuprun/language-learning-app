145caab49e5028d2a25d1f67fab1857d954624b6
Yurii_Suprun <Yurii_Suprun@gmail.com>
2024-08-13 11:30:40 +0200

    Add User service

---
+++ user-management-service/src/main/java/com/example/user_management_service/service/UserService.java
@@ -0,0 +1,31 @@
+package com.example.user_management_service.service;
+
+import com.example.user_management_service.model.User;
+import com.example.user_management_service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Yurii_Suprun
+ */
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public User registerUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole("USER"); // default role
+        return userRepository.save(user);
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}
+
