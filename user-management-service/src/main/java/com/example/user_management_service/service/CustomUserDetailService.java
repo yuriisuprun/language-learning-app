361c2e11683c67098476abab7f8438b7cc80d2cc
Yurii_Suprun <Yurii_Suprun@gmail.com>
2024-08-13 17:05:06 +0200

    Update SecurityConfiguration

---
+++ user-management-service/src/main/java/com/example/user_management_service/service/CustomUserDetailService.java
@@ -0,0 +1,45 @@
+package com.example.user_management_service.service;
+
+import com.example.user_management_service.model.CustomUser;
+import com.example.user_management_service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+/**
+ * @author Yurii_Suprun
+ */
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<CustomUser> user = repository.findByUsername(username);
+        if (user.isPresent()) {
+            var userObj = user.get();
+            return User.builder()
+                    .username(userObj.getUsername())
+                    .password(userObj.getPassword())
+                    .roles(getRoles(userObj))
+                    .build();
+        } else {
+            throw new UsernameNotFoundException(username);
+        }
+    }
+
+    private String[] getRoles(CustomUser user) {
+        if (user.getRole() == null) {
+            return new String[]{"USER"};
+        }
+        return user.getRole().split(",");
+    }
+}
+
