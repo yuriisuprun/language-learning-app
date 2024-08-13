4a1349f4800a1ab6e6d7d37e51c92048b91bea79
Yurii_Suprun <Yurii_Suprun@gmail.com>
2024-08-13 12:42:34 +0200

    Update UserService

--- user-management-service/src/main/java/com/example/user_management_service/service/UserService.java
+++ user-management-service/src/main/java/com/example/user_management_service/service/UserService.java
@@ -3,7 +3,7 @@ package com.example.user_management_service.service;
 import com.example.user_management_service.model.User;
 import com.example.user_management_service.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,8 +15,13 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    //    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Autowired
-    private BCryptPasswordEncoder passwordEncoder;
+    public UserService(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     public User registerUser(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
