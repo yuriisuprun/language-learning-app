9398b72c4328f94e824e1fffbd9d9e12b56090f5
Yurii_Suprun <Yurii_Suprun@gmail.com>
2024-08-13 17:37:43 +0200

    Update CustomUser

--- user-management-service/src/main/java/com/example/user_management_service/model/CustomUser.java
+++ user-management-service/src/main/java/com/example/user_management_service/model/CustomUser.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import lombok.Data;
 
 /**
@@ -14,7 +15,8 @@ import lombok.Data;
 public class CustomUser {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "custom_user_seq")
+    @SequenceGenerator(name = "custom_user_seq", sequenceName = "custom_user_seq", allocationSize = 1)
     private Long id;
     private String username;
     private String password;
