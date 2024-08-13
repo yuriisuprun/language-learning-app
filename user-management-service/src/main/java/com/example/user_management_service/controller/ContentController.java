eb3d97de4beb38ff479ebbb3752961cff774ab36
Yurii_Suprun <Yurii_Suprun@gmail.com>
2024-08-13 22:58:20 +0200

    Update ContentController

--- user-management-service/src/main/java/com/example/user_management_service/controller/ContentController.java
+++ user-management-service/src/main/java/com/example/user_management_service/controller/ContentController.java
@@ -9,20 +9,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ContentController {
 
-//    @GetMapping("/home")
-//    public String handleWelcome() {
-//        return "home";
-//    }
-//
-//    @GetMapping("/admin/home")
-//    public String handleAdminHome() {
-//        return "home_admin";
-//    }
-//
-//    @GetMapping("/user/home")
-//    public String handleUserHome() {
-//        return "home_user";
-//    }
+    @GetMapping("/home")
+    public String handleWelcome() {
+        return "home";
+    }
+
+    @GetMapping("/admin/home")
+    public String handleAdminHome() {
+        return "home_admin";
+    }
+
+    @GetMapping("/user/home")
+    public String handleUserHome() {
+        return "home_user";
+    }
 
     @GetMapping("/login")
     public String handleLogin() {
